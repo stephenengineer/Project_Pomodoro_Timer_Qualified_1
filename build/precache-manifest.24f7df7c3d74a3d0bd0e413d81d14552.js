ec1ab00c9acab43df4709f422c3f9661bcfa2639
stephenengineer <stephen.tchaou@alumni.stonybrook.edu>
2021-06-23 05:29:32 -0700

    Added build and deploy scripts and ran build

---
+++ build/precache-manifest.24f7df7c3d74a3d0bd0e413d81d14552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31acd0d96e9b46a2b04842774fb264af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e00f1ad13ce1116da73",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e00f1ad13ce1116da73",
+    "url": "/static/js/main.a2d56c3c.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
